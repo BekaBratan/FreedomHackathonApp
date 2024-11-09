41ad678a2ef5051af47676fc824ec4002bf86f81
Assanalikhan <210103042@mail.com>
2024-11-09 11:17:25 +0500

    ViewModels

--- app/src/main/java/com/example/freedomhackathonapp/data/ApiService.kt
+++ app/src/main/java/com/example/freedomhackathonapp/data/ApiService.kt
@@ -1,10 +1,11 @@
 package com.example.freedomhackathonapp.data
 
+import com.example.freedomhackathonapp.domain.VacancyResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
 
 interface ApiService {
 
-    @GET("api/products")
-    suspend fun getProducts(): List<String>
+    @GET("api/vacancy")
+    suspend fun getVacancy(): List<VacancyResponse>
 }
--- app/src/main/java/com/example/freedomhackathonapp/presentaion/search/SearchViewModel.kt
+++ app/src/main/java/com/example/freedomhackathonapp/presentaion/search/SearchViewModel.kt
@@ -6,18 +6,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.freedomhackathonapp.data.ApiService
 import com.example.freedomhackathonapp.data.ServiceBuilder
+import com.example.freedomhackathonapp.domain.VacancyResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class SearchViewModel: ViewModel() {
-    private val _response = MutableLiveData<String>()
-    val response: LiveData<String> get() = _response
+    private val _response = MutableLiveData<List<VacancyResponse>>()
+    val response: LiveData<List<VacancyResponse>> get() = _response
 
     private val apiService = ServiceBuilder.buildService(ApiService::class.java)
 
     fun getResponse() {
         viewModelScope.launch(Dispatchers.IO) {
-            _response.value = apiService.getProducts().toString()
+            _response.value = apiService.getVacancy()
         }
     }
 }
